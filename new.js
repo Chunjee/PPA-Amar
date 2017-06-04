4ec5e06d3d3aaea5bfb2eb3380d8587e291c61ca
Chunjee <ngachunjee@gmail.com>
2017-06-04 05:11:27 -0700

    chore: Update data.json for 06.04.17

--- new.js
+++ new.js
@@ -235,7 +235,7 @@ var fn_AllChart = function () {
     console.log(typeof (AllChart));
     //update chart data if already created
     if (typeof (AllChart) == "object") {
-        console.log("ALF!!!");
+        //console.log("ALF!!!");
         AllChart.config.data = data;
         AllChart.update();
     }
@@ -384,7 +384,7 @@ var fn_ParseAllCharacterNames = function (para_InputJSON) {
 //Select a RGB color for input
 var fn_MakeRGB = function (para_Input, para_Guild) {
     if (para_Guild === void 0) { para_Guild = "alf"; }
-    var Goon_Guilds = ["(Glass)", "(#10)", "(pine)", "(abcdegh)", "(~nets~)", "(Fruit!)", "(EGGS)", "(NaCl)", "(cone?)", "(Pepsi.)", "(****)", "(:coal:)", "(MULTIS)", "(YAMS)", "(goose)", "(~worms~)", "(cone)", "(GOONS)"];
+    var Goon_Guilds = ["(MEAT)", "(Glass)", "(#10)", "(pine)", "(abcdegh)", "(~nets~)", "(Fruit!)", "(EGGS)", "(NaCl)", "(cone?)", "(Pepsi.)", "(****)", "(:coal:)", "(MULTIS)", "(YAMS)", "(goose)", "(~worms~)", "(cone)", "(GOONS)"];
     if (Goon_Guilds.indexOf(para_Guild) != -1) {
         return "rgba(0,0,0,";
     }
